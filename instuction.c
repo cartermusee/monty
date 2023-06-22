988ec5421893b9605a948bd2b64853afced2da5a
cartermusee <cartermusee@gmail.com>
2023-06-22 04:04:16 -0700

    multiply 2 top elements

--- instuction.c
+++ instuction.c
@@ -17,8 +17,8 @@ int instruct(char *line, stack_t **stack, unsigned int num, FILE *f)
 		{"swap", swap2elements},
 		{"add", add2node},
 		{"nop", nopnode}, {"sub", subnode},
-		{"div", div2node},/*
-		{"mul", mul},
+		{"div", div2node},
+		{"mul", mul},/*
 		{"mod", mod},*/
 		{NULL, NULL}
 	};
---
+++ mul.c
@@ -0,0 +1,31 @@
+#include "mont.h"
+/**
+ * mul - function to multiply
+ * @head:head of stack
+ * @num:line count
+ * Return:nothing
+*/
+void mul(stack_t **head, unsigned int num)
+{
+	stack_t *temp;
+	int i = 0, mul;
+
+	temp = *head;
+	while (temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (i < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", num);
+		fclose(varies.f);
+		free(varies.line);
+		freem(*head);
+		exit(EXIT_FAILURE);
+	}
+	mul = temp->next->n * temp->n;
+	temp->next->n = mul;
+	*head = temp->next;
+	free(temp);
+}
