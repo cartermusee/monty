f1288b48f086db7f7338650fcd766c0e17fa4996
cartermusee <cartermusee@gmail.com>
2023-06-22 03:46:24 -0700

    add 2 elements

---
+++ add.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * add2node-function which adds two nodes
+ * @head:head of a stack
+ * @num:number
+ * Return:nothing
+*/
+void add2node(stack_t **head, unsigned int num)
+{
+	stack_t *temp;
+	int sum, i = 0;
+	
+	temp = *head;
+	while (temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (i < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", num);
+		fclose(varies.f);
+		free(varies.line);
+		freem(*head);
+		exit(EXIT_FAILURE);
+	}
+	sum = temp->n + temp->next->n;
+	temp->next->n = sum;
+	*head = temp->next;
+	free(temp);
+}
--- instuction.c
+++ instuction.c
@@ -14,8 +14,8 @@ int instruct(char *line, stack_t **stack, unsigned int num, FILE *f)
 
 	instruction_t set[] = {{"push", push}, {"pall", pal}, {"pint", peek},
 		{"pop", pop},
-		{"swap", swap2elements},/*
-		{"add", add2node},
+		{"swap", swap2elements},
+		{"add", add2node},/*
 		{"nop", nopnode}, {"sub", subnode},
 		{"div", div2node},
 		{"mul", mul},
